0fda6983501eba318b633d18a7ae5a2666c42251
augusto lourenço <augustojunior8@gmail.com>
2025-12-23 23:36:03 +0000

    essa é a chave api gemini: AIzaSyD0mAnHytxOe0ZC0K3-bIFPTdVCXnnJY8M

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -6,10 +6,7 @@ export const flash = 'googleai/gemini-flash-latest';
 
 export const ai = genkit({
   plugins: [
-    googleAI({
-      // Defina a versão da API explicitamente para evitar avisos.
-      apiVersion: 'v1beta',
-    }),
+    googleAI(),
   ],
   // Set the default, fast model
   model: flash,
